69f4f449451f66e4fd4d4525be20925f66770fe5
Hurin Vlad <longsno@gmail.com>
2021-08-14 03:05:50 +0300

    feat: pop-up added

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainBlockComponent } from './main-block/main-block.component';
 import { CardComponent } from './card/card.component';
+import { MoreInfoComponent } from './more-info/more-info.component';
 
 
 
 @NgModule({
   declarations: [
     MainBlockComponent,
-    CardComponent
+    CardComponent,
+    MoreInfoComponent
   ],
   imports: [
     CommonModule
