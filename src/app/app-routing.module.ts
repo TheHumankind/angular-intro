9be91f03819a30ce11161b75e089d8a2e1994bcd
Hurin Vlad <longsno@gmail.com>
2021-08-28 17:00:42 +0300

    feat: admin page implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPageComponent } from './admin/admin-page/admin-page.component';
 import { LoginBlockComponent } from './login-pop-up/login-block/login-block.component';
 import { MainBlockComponent } from './main/main-block/main-block.component';
 import { BlockComponent } from './page-404/block/block.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: LoginBlockComponent },
   { path: 'login', component: LoginBlockComponent },
   { path: 'main', component: MainBlockComponent },
+  { path: 'admin', component: AdminPageComponent },
   { path: 'error', component: BlockComponent },
   { path: 'main/:id', component: MainBlockComponent },
   { path: 'main', redirectTo: 'error' },  
